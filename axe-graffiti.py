f6f70e5a078285ca757472ca241c558ad24f06fa
-k <slowdive@me.com>
2018-07-01 00:30:17 -0400

    Update axe-graffiti.py

--- axe-graffiti.py
+++ axe-graffiti.py
@@ -4,7 +4,7 @@
 from lib.graffiti import generate_graffiti_address
 
 
-string = "The message."
+string = "Zero"
 
 print('Write graffiti "' + string + '" to blockchain by sending some haks to:\n')
 print(generate_graffiti_address(string))
